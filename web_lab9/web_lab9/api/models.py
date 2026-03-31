5e8801e70c81ca664d372f141a823890ff9e93a7
lya33at <l_nurperzentova@kbtu.kz>
2026-03-31 14:56:46 +0500

    Add files via upload

---
+++ web_lab9/web_lab9/api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
